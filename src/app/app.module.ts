345bbb0dc72bc76a1d5739cc6407000a879ed848
Miguel Valero <miguel.valero@upc.edu>
2018-12-24 10:26:21 +0100

    Final leccion 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
 import { PersonaComponent } from './persona/persona.component';
+import { PersonasComponent } from './personas/personas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculadoraComponent,
-    PersonaComponent
+    PersonaComponent,
+    PersonasComponent
   ],
   imports: [
     BrowserModule,
